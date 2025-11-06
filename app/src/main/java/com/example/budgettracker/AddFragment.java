98ab3e51aefa1136d63abc209f44f8a1d0bb162b
tom <tgilholm@gmail.com>
2025-11-06 15:30:51 +0000

    Added layout in fragment_add.xml

--- app/src/main/java/com/example/budgettracker/AddFragment.java
+++ app/src/main/java/com/example/budgettracker/AddFragment.java
@@ -1,6 +1,9 @@
 package com.example.budgettracker;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +17,16 @@ import android.view.ViewGroup;
 public class AddFragment extends Fragment {
 
     /*
-    TODO: Add Task Functionality- save new transaction to persistent storage, update all screens
+    TODO: Add Functionality- save new transaction to persistent storage, update all screens
     If connected to cloud, save to cloud.
      */
+    /*
+    TODO: Chip group containing chips representing each category- default categories & user defined
+     */
+
+    // TODO DialogFragment displaying information to input date and time of transaction
+    // By default today's date is already inputted whenever the fragment is loaded
+    // The user can then tap on the date or the time and it will open a popup prompting them to change it
 
     public AddFragment() {
         // Required empty public constructor
@@ -42,5 +52,14 @@ public class AddFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+
+    // Handle the logic for the fragment startup
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Set the current date & time in the textEdit fields
+
     }
 }
